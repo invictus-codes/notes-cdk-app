7e1c7e41e0a91d0d50668b917995bef9ce1765fa
Jens van Hellemondt <jens@invictus.codes>
2023-03-25 22:50:48 +0100

    feat: create cdk stack

---
+++ infra/src/assets/lambda/notes/main.ts
@@ -0,0 +1,33 @@
+import Note from "./types/Note";
+import createNote from "./resolvers/createNote";
+import deleteNote from "./resolvers/deleteNote";
+import getNoteById from "./resolvers/getNoteById";
+import listNotes from "./resolvers/listNotes";
+import updateNote from "./resolvers/updateNote";
+
+type AppSyncEvent = {
+    info: {
+        fieldName: string;
+    };
+    arguments: {
+        noteId: string;
+        note: Note;
+    };
+};
+
+export async function handler(event: AppSyncEvent) {
+    switch (event.info.fieldName) {
+        case "createNote":
+            return await createNote(event.arguments.note);
+        case "getNoteById":
+            return await getNoteById(event.arguments.noteId);
+        case "listNotes":
+            return await listNotes();
+        case "updateNote":
+            return await updateNote(event.arguments.note);
+        case "deleteNote":
+            return await deleteNote(event.arguments.noteId);
+        default:
+            return null;
+    }
+}
---
+++ infra/src/assets/lambda/notes/resolvers/createNote.ts
@@ -0,0 +1,19 @@
+const AWS = require("aws-sdk");
+import Note from "../types/Note";
+
+const docClient = new AWS.DynamoDB.DocumentClient();
+
+async function createNote(note: Note) {
+    const params = {
+        TableName: process.env.NOTES_TABLE,
+        Item: note,
+    };
+    try {
+        await docClient.put(params).promise();
+        return note;
+    } catch (err) {
+        console.log("DynamoDB error: ", err);
+        return null;
+    }
+}
+export default createNote;
